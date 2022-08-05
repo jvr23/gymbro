31445a34fccb0b41381bea320403590f79e3a598
Javier Aguirre <javieraguirre@live.com>
2022-08-05 10:39:58 -0500

    change the order of the migrations because a migration referenced a table that is created later

--- db/migrate/20220728140940_remove_owner_from_gyms.rb
+++ db/migrate/20220728140940_remove_owner_from_gyms.rb
@@ -1,6 +1,6 @@
 class RemoveOwnerFromGyms < ActiveRecord::Migration[7.0]
   def change
-    remove_column :gyms, :owner_id
+    remove_column :gyms, :owner_id, :int
     # remove_reference :gyms, :owner_id, null: false, foreign_key: true
   end
 end
